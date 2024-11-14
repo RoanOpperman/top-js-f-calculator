a7ee1ac9efc6388c778f2adbc544c4ac08ea5032
Roan <17789917+RoanOpperman@users.noreply.github.com>
2024-11-13 17:34:06 -0700

    [layout]: add basic html structure and temporary css styling

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict";
 console.log("connected");
+//* ------------------    START: STYLING    ------------------
+// const inputContainer = document.querySelector("#input");
+// const stylingInputRow = function () {
+//   for (let i = 1; i <= 5; i++) {
+//     const rowDiv = document.createElement("div");
+//     rowDiv.classList.add("input-row");
+//     for (let r = 1; r <= 4; r++) {
+//       const colDiv = document.createElement("div");
+//       colDiv.classList.add("input-column");
+//       rowDiv.appendChild(colDiv);
+//     }
+//     inputContainer.appendChild(rowDiv);
+//   }
+// };
+// stylingInputRow();
+//* ------------------    END: STYLING      ------------------
