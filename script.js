54042e5fa9c1cf0afbb7eeae733e6c361b67d5a0
Roan <17789917+RoanOpperman@users.noreply.github.com>
2024-11-20 09:57:56 -0700

    add initial javascript as per the odin project project description

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 "use strict";
 console.log("connected");
-//* ------------------    START: STYLING    ------------------
-
-//* ------------------    END: STYLING      ------------------
+//* --------------------------------------------------
+//* -------------   Computational logic  -------------
+//* --------------------------------------------------
+const addition = function (a, b) {}; //return the result
+const subtraction = function (a, b) {}; //return the result
+const multiplication = function (a, b) {}; //return the result
+const division = function (a, b) {}; //return the result
+const handleFloatPointNumbers = function () {};
+const operate = function (operator, a, b) {}; // Calls on functions (addition, subtraction, multiplication, and division) depending on what operator was used
+//* --------------------------------------------------
+//* -------------        UI logic        -------------
+//* --------------------------------------------------
+const getFirstNumber = function () {};
+const getSecondNumber = function () {};
+const getOperator = function () {};
+const backspace = function () {};
+const keyboardActions = function () {}; // May break this into more/smaller functions
+const populateDisplay = function () {
+  // populate the display when digit buttons are clicked
+  // store the content of the display (the number) in a variable for later use
+  // use the get functions (ex. getFirstNumber) as parameters for this function (inputs)
+  // after calling the operate function, update the display
+};
